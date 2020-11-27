380faae043edd43211fac57003598936c9ac1552
VonC <vonc@laposte.net>
2020-11-27 12:42:08 +0100

    Display version on -v, --version or version as parameter

--- main.go
+++ main.go
@@ -2,11 +2,23 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
 
+	"github.com/VonC/username/version"
 	"github.com/eiannone/keyboard"
 )
 
 func main() {
+
+	for _, f := range os.Args[1:] {
+		fl := strings.ToLower(f)
+		if fl == "-v" || fl == "--version" || fl == "version" {
+			fmt.Println(version.String())
+			os.Exit(0)
+		}
+	}
+
 	keysEvents, err := keyboard.GetKeys(10)
 	if err != nil {
 		panic(err)
