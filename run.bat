0f4b674a0751f24643366ecd6023156a78dada1e
VonC <vonc@laposte.net>
2020-11-27 12:38:03 +0100

    Add run.bat

---
+++ run.bat
@@ -0,0 +1,16 @@
+@echo off
+setlocal enabledelayedexpansion
+
+for %%i in ("%~dp0.") do SET "script_dir=%%~fi"
+cd "%script_dir%"
+for %%i in ("%~dp0.") do SET "dirname=%%~ni"
+
+REM https://stackoverflow.com/questions/16354102/how-to-get-the-rest-of-arguments-in-windows-batch-file
+REM https://stackoverflow.com/a/16354963/6309
+SET allargs=%*
+SET arg1=%1
+CALL SET "someargs=%%allargs:*%1=%%"
+%dirname% %allargs%
+set err=%ERRORLEVEL%
+echo.
+echo ERRORLEVEL (exit status)='%err%'
