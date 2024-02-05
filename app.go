81364be3ac8a81ddfabcaceb5030e4fb42ad1cc7
VonC <vonc@laposte.net>
2024-02-05 16:04:35 +0100

    App charmbracelt huh first implementation

---
+++ app.go
@@ -0,0 +1,160 @@
+package main
+
+// https://github.com/charmbracelet/huh/blob/main/examples/bubbletea/main.go
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/huh"
+	"github.com/charmbracelet/lipgloss"
+)
+
+func mainApp() {
+	_, err := tea.NewProgram(NewModel()).Run()
+	if err != nil {
+		fmt.Println("Oh no:", err)
+		os.Exit(1)
+	}
+}
+
+type Model struct {
+	lg        *lipgloss.Renderer
+	styles    *Styles
+	form      *huh.Form
+	width     int
+	firstname string
+	lastname  string
+}
+
+const maxWidth = 80
+
+var (
+	red    = lipgloss.AdaptiveColor{Light: "#FE5F86", Dark: "#FE5F86"}
+	indigo = lipgloss.AdaptiveColor{Light: "#5A56E0", Dark: "#7571F9"}
+	green  = lipgloss.AdaptiveColor{Light: "#02BA84", Dark: "#02BF87"}
+)
+
+type Styles struct {
+	Base,
+	HeaderText,
+	Status,
+	StatusHeader,
+	Highlight,
+	ErrorHeaderText,
+	Help lipgloss.Style
+}
+
+func NewStyles(lg *lipgloss.Renderer) *Styles {
+	s := Styles{}
+	s.Base = lg.NewStyle().
+		Padding(1, 4, 0, 1)
+	s.HeaderText = lg.NewStyle().
+		Foreground(indigo).
+		Bold(true).
+		Padding(0, 1, 0, 2)
+	s.Status = lg.NewStyle().
+		Border(lipgloss.RoundedBorder()).
+		BorderForeground(indigo).
+		PaddingLeft(1).
+		MarginTop(1)
+	s.StatusHeader = lg.NewStyle().
+		Foreground(green).
+		Bold(true)
+	s.Highlight = lg.NewStyle().
+		Foreground(lipgloss.Color("212"))
+	s.ErrorHeaderText = s.HeaderText.Copy().
+		Foreground(red)
+	s.Help = lg.NewStyle().
+		Foreground(lipgloss.Color("240"))
+	return &s
+}
+
+func NewModel() Model {
+	m := Model{width: maxWidth}
+	m.lg = lipgloss.DefaultRenderer()
+	m.styles = NewStyles(m.lg)
+
+	m.form = huh.NewForm(
+		huh.NewGroup(
+			huh.NewInput().
+				Value(&m.firstname).
+				Title("Firstname"),
+			huh.NewInput().
+				Value(&m.lastname).
+				Title("Lastname"),
+			huh.NewText().
+				Title("Result Windows logins"),
+		),
+	).
+		WithWidth(45).
+		WithShowHelp(false).
+		WithShowErrors(false)
+	return m
+}
+
+// Init implements tea.Model.
+func (m Model) Init() tea.Cmd {
+	return m.form.Init()
+}
+
+// Update implements tea.Model.
+func (m Model) Update(tea.Msg) (tea.Model, tea.Cmd) {
+	return m, nil
+}
+
+// View implements tea.Model.
+func (m Model) View() string {
+	s := m.styles
+
+	v := strings.TrimSuffix(m.form.View(), "\n\n")
+	form := m.lg.NewStyle().Margin(1, 0).Render(v)
+
+	errors := m.form.Errors()
+	header := m.appBoundaryView("Charm Employment Application")
+	if len(errors) > 0 {
+		header = m.appErrorBoundaryView(m.errorView())
+	}
+	resLogins := "test res login"
+	body := lipgloss.JoinHorizontal(lipgloss.Top, form, resLogins)
+
+	footer := m.appBoundaryView(m.form.Help().ShortHelpView(m.form.KeyBinds()))
+	if len(errors) > 0 {
+		footer = m.appErrorBoundaryView("")
+	}
+
+	return s.Base.Render(header + "\n" + body + "\n\n" + footer)
+}
+
+// errorView returns a string representation of the errors in the Model's form.
+// It iterates over the errors and concatenates their error messages into a single string.
+// Returns: a string containing the error messages.
+func (m Model) errorView() string {
+	var s string
+	for _, err := range m.form.Errors() {
+		s += err.Error()
+	}
+	return s
+}
+
+func (m Model) appBoundaryView(text string) string {
+	return lipgloss.PlaceHorizontal(
+		m.width,
+		lipgloss.Left,
+		m.styles.HeaderText.Render(text),
+		lipgloss.WithWhitespaceChars("/"),
+		lipgloss.WithWhitespaceForeground(indigo),
+	)
+}
+
+func (m Model) appErrorBoundaryView(text string) string {
+	return lipgloss.PlaceHorizontal(
+		m.width,
+		lipgloss.Left,
+		m.styles.ErrorHeaderText.Render(text),
+		lipgloss.WithWhitespaceChars("/"),
+		lipgloss.WithWhitespaceForeground(red),
+	)
+}
--- go.mod
+++ go.mod
@@ -1,10 +1,34 @@
 module github.com/VonC/winlogin
 
-go 1.15
+go 1.21
 
 require (
-	github.com/atotto/clipboard v0.1.2
+	github.com/atotto/clipboard v0.1.4
+	github.com/charmbracelet/bubbletea v0.25.0
+	github.com/charmbracelet/huh v0.3.0
+	github.com/charmbracelet/lipgloss v0.9.1
 	github.com/eiannone/keyboard v0.0.0-20200508000154-caf4b762e807
 	github.com/vbauerster/mpb/v5 v5.3.0
-	golang.org/x/sys v0.1.0 // indirect
+)
+
+require (
+	github.com/VividCortex/ewma v1.1.1 // indirect
+	github.com/acarl005/stripansi v0.0.0-20180116102854-5a71ef0e047d // indirect
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/catppuccin/go v0.2.0 // indirect
+	github.com/charmbracelet/bubbles v0.17.2-0.20240108170749-ec883029c8e6 // indirect
+	github.com/containerd/console v1.0.4-0.20230313162750-1ae8d489ac81 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-localereader v0.0.1 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/muesli/ansi v0.0.0-20230316100256-276c6243b2f6 // indirect
+	github.com/muesli/cancelreader v0.2.2 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.15.2 // indirect
+	github.com/rivo/uniseg v0.4.4 // indirect
+	golang.org/x/sync v0.4.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/term v0.13.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
 )
